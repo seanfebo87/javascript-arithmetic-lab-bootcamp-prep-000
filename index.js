9a0d0ee6ae9db01197f62b6ce2afef54343b155a
Sean Febo <seanfebo@techie.com>
2018-03-06 04:42:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function divide(a,b) {
   return (a / b);
 }
 function inc(n) {
-  return n++;
+  return n += 1;
 }
 function inc(n) {
-  return n--;
+  return n -= 1;
 }
 function makeInt(n) {
   return parseInt(n, 10);
 }
 function preserveDecimal(n) {
-  return parseFloat(n)
+  return parseFloat(n);
 }
